04e5319d0d6d57e300d06a8cf2f704abf18266aa
Edmundo Nascimento <edmundo.nascimento@gerdau.com>
2024-06-01 08:18:37 -0300

    chore: first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="main">
+      <h1>Edmundo Blog</h1>
+      <h2>Edmundo Blog</h2>
+      <h3>Edmundo Blog</h3>
+      <h4>Edmundo Blog</h4>
+      <h5>Edmundo Blog</h5>
+      <h6>Edmundo Blog</h6>
+      <p>Edmundo Blog</p>
+      <span>Edmundo Blog</span>
+    </div>
+  );
+}
